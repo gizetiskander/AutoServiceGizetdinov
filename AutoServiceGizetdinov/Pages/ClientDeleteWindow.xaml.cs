18939a9e49910b80ae571aa7230a8b13382011e0
Гизетдинов Искандер <gizetiskander@gmail.com>
2023-03-16 11:56:52 +0300

    db and windows

--- AutoServiceGizetdinov/Pages/ClientDeleteWindow.xaml.cs
+++ AutoServiceGizetdinov/Pages/ClientDeleteWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AutoServiceGizetdinov
+namespace AutoServiceGizetdinov.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ClientDeleteWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ClientDeleteWindow : Window
     {
-        public MainWindow()
+        public ClientDeleteWindow()
         {
             InitializeComponent();
         }
