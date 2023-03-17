724d015f6c8f6b4baa9d42c15c26c1083b134f00
Гизетдинов Искандер <gizetiskander@gmail.com>
2023-03-17 18:52:41 +0300

    delwindow is done

--- AutoServiceGizetdinov/Pages/ClientDeleteWindow.xaml.cs
+++ AutoServiceGizetdinov/Pages/ClientDeleteWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoServiceGizetdinov.db;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,42 @@ namespace AutoServiceGizetdinov.Pages
     /// </summary>
     public partial class ClientDeleteWindow : Window
     {
+        public static CrashAuto_dbEntities dbEntities = new CrashAuto_dbEntities();
         public ClientDeleteWindow()
         {
             InitializeComponent();
+            ClientLst.ItemsSource = dbEntities.Client.ToList();
+        }
+
+        private void DelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var q = ClientLst.SelectedItem as Client;
+            if (q == null)
+            {
+                MessageBox.Show("Ничего не выбрано!");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите удалить строку?", "Удалить?", MessageBoxButton.YesNoCancel);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dbEntities.Client.Remove(q);
+                    ClientLst.ItemsSource = dbEntities.Client.ToList();
+                    dbEntities.SaveChanges();
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка!");
+                }
+            }
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClientListWindow clientListWindow = new ClientListWindow();
+            clientListWindow.Show();
+            this.Close();
         }
     }
 }
